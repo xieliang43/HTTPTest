858a78ecccc743e30660aff7278001c31f770048
Albert <xieliang43@126.com>
2012-06-25 16:22:19 +0800

    init project

---
+++ HTTPTest/XLDocument.h
@@ -0,0 +1,13 @@
+//
+//  XLDocument.h
+//  HTTPTest
+//
+//  Created by xie liang on 6/25/12.
+//  Copyright (c) 2012 pretang. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface XLDocument : NSDocument
+
+@end
---
+++ HTTPTest/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  HTTPTest
+//
+//  Created by xie liang on 6/25/12.
+//  Copyright (c) 2012 pretang. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
